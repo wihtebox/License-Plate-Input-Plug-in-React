4eb30113e6687b0fb75e8f21f887afe9c8b289c1
Mr.wei <846484950@qq.com>
2019-08-06 10:01:44 +0800

    完善版本更新

--- src/components/index/Chepai.js
+++ src/components/index/Chepai.js
@@ -3,7 +3,7 @@ import './Chepai.css';
 import choose from '../../dist/img/choose.svg'
 import choosed from '../../dist/img/choosed.svg'
 const inputContent = {
-	provinces:["京","沪","浙","苏","粤","鲁","晋","冀","豫","川","渝","辽","吉","黑","皖","鄂","津","贵","云","桂","琼","青","新","藏","蒙","宁","甘","陕","闽","赣","湘"],
+	provinces:["桂","京","沪","浙","苏","粤","鲁","晋","冀","豫","川","渝","辽","吉","黑","皖","鄂","津","贵","云","琼","青","新","藏","蒙","宁","甘","陕","闽","赣","湘"],
 	keyNums:["0","1","2","3","4","5","6","7","8","9","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","确定","Z","X","C","V","B","N","M","删除"],
 }
 
@@ -12,101 +12,218 @@ class Chepai extends Component {
 		super(props);
 		this.state={
 			open:this.props.open,
-			energy:false,//是否是新能源
-			license:['','','','','','','',''],
-			inputKey:0,//输入的序号
-			selectEnergy:choose
+			energy:this.props.energy===0?false:true,//是否是新能源
+			license:['-','-','-','-','-','-','-','-'],
+			inputKey:this.props.inputKey||0,//输入的序号
+			selectEnergy:choose,
+			bottom:this.props.bottom||'0px',//离底部的高度
+			zIndex:this.props.zIndex||'0',
+			status:"input",//input:输入状态，delete:删除状态
 		};
+
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.open){
+			this.setState({
+				open:true,
+			})
+		}else{
+			this.setState({
+				open:false,
+			})
+		}
+
+		let license = '';
+		console.log(nextProps.license)
+		if(nextProps.license===""){
+			license = ['-','-','-','-','-','-','-','-'];
+		}else{
+			license = (nextProps.license).split('');
+		}
+		if(nextProps.inputKey===0){
+			license = (nextProps.license).split('');
+		}
+		if(!nextProps.energy){
+			license[7]='';
+		}
+
+		this.setState({
+			energy: nextProps.energy,
+			license:license,
+			inputKey:nextProps.inputKey
+		})
 	}
 	returnProps=()=>{
 		let license=''
+		console.log(this.state.license)
 		this.state.license.map((value,i)=>{
-			value===''?'':license+=value;
+			let res = value===''?'':license+=value;
 		})
-		this.props.onChange({license:license,energy:this.state.energy,index:this.state.inputKey})
+		this.props.onChange({license:license,open:this.state.open,inputKey:this.state.inputKey})
 	}
-	clickBox=(key)=>{
-		this.setState({
-			inputKey:key,
-			open:true
-		})
-	}
+
 	chooseProvince=(val)=>{
 		let license = this.state.license;
 		license[0] = val;
-	
+		let key;
+		if((this.state.inputKey<7&&this.state.energy)||(this.state.inputKey<6&&!this.state.energy)){
+			console.log("454554")
+			key=this.state.inputKey+1;
+		}else if(!this.state.energy&&this.state.inputKey===6){
+			console.log("454554")
+			key=this.state.inputKey;
+		}
+
 		this.setState({
 			license:license,
-			inputKey:this.state.inputKey+1
-		},this.returnProps())
-		
+			inputKey:key,
+			status:"input",
+		},()=>this.returnProps())
+
 	}
 	choosekey=(val)=>{
 		let license = this.state.license;
+		//删除操作
 		if(val==='删除'){
-			license[this.state.inputKey] = '';
-			license[this.state.inputKey-1] = '';
+			let inputKey=this.state.inputKey;
+			license[inputKey]="-";
 			this.setState({
 				license:license,
-				inputKey:this.state.inputKey-1
-			},this.returnProps())
-			
+				inputKey:inputKey>0?inputKey-1:inputKey,
+				status:"delete",
+			},()=>{console.log(this.state.inputKey);this.returnProps()})
 			return;
 		}
+		//确定操作
 		if(val==='确定'){
 			this.setState({
-				open:!this.state.open
-			})
-			this.returnProps()
+				open:false,
+			},()=>this.returnProps())
 			return;
 		}
-		if(!this.state.energy&&this.state.inputKey===7){
-			this.setState({
-				open:!this.state.open
-			})
-			return;
+		//非新能源 输入信息，字母和数字
+		if(!this.state.energy&&val!=='删除'&&val!=='确定'){
+			// 根据输入状态作不同操作
+			//输入状态
+			if(this.state.status==="input"){
+				if(this.state.inputKey<6){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey===6?this.state.inputKey:this.state.inputKey+1,
+					},()=>this.returnProps())
+				}
+				if(this.state.inputKey===6){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey,
+					},()=>this.returnProps())
+				}
+				if(this.state.inputKey===7){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey,
+					},()=>this.returnProps())
+				}
+			}
+			// 删除状态
+			if(this.state.status==="delete"){
+				if(license[this.state.inputKey]==='-'){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey+1,
+						status:"input",
+					},()=>this.returnProps())
+				}else{
+					if(this.state.inputKey<5){
+						license[this.state.inputKey+1] = val;
+						this.setState({
+							license:license,
+							inputKey:this.state.inputKey+2,
+							status:"input",
+						},()=>this.returnProps())
+					}
+					if(this.state.inputKey===5){
+						license[this.state.inputKey+1] = val;
+						this.setState({
+							license:license,
+							inputKey:this.state.inputKey+1,
+							status:"input",
+						},()=>this.returnProps())
+					}
+					if(this.state.inputKey===6){
+						license[this.state.inputKey+1] = val;
+						this.setState({
+							license:license,
+							inputKey:this.state.inputKey+1,
+							status:"input",
+						},()=>this.returnProps())
+					}
+				}
+
+			}
 		}
-		if(this.state.energy&&this.state.inputKey===8){
-			this.setState({
-				open:!this.state.open
-			})
-			return;
-		}
-		if(!this.state.energy&&this.state.inputKey<7&&val!=='删除'&&val!=='确定'){
-			license[this.state.inputKey] = val;
-			this.setState({
-				license:license,
-				inputKey:!this.state.energy&&this.state.inputKey<6?this.state.inputKey+1:this.state.inputKey
-			},this.returnProps())
-		}else if(this.state.inputKey<8&&val!=='删除'&&val!=='确定'){
-			license[this.state.inputKey] = val;
-			this.setState({
-				license:license,
-				inputKey:this.state.energy&&this.state.inputKey<7?this.state.inputKey+1:this.state.inputKey
-			},this.returnProps())
-		}else{
-			this.setState({
-				open:false
-			})
+		//非能源 输入信息，字母和数字
+		if(this.state.energy&&val!=='删除'&&val!=='确定'){
+			if(this.state.status==="input"){
+				if(this.state.inputKey<7){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey===7?this.state.inputKey:this.state.inputKey+1,
+					},()=>this.returnProps())
+				}
+				if(this.state.inputKey===7){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey,
+					},()=>this.returnProps())
+				}
+			}
+			if(this.state.status==="delete"){
+				if(license[this.state.inputKey]==='-'){
+					license[this.state.inputKey] = val;
+					this.setState({
+						license:license,
+						inputKey:this.state.inputKey+1,
+						status:"input",
+					},()=>this.returnProps())
+				}else{
+					if(this.state.inputKey<6){
+						license[this.state.inputKey+1] = val;
+						this.setState({
+							license:license,
+							inputKey:this.state.inputKey+2,
+							status:"input",
+						},()=>this.returnProps())
+					}
+					if(this.state.inputKey===6){
+						license[this.state.inputKey+1] = val;
+						this.setState({
+							license:license,
+							inputKey:this.state.inputKey+1,
+							status:"input",
+						},()=>this.returnProps())
+					}
+				}
+			}
 		}
 	}
-	selectEnergy=()=>{
-		let license = this.state.license;
-		this.state.selectEnergy===choose?license[7]='':''
-		this.setState({
-			selectEnergy:this.state.selectEnergy===choose?choosed:choose,
-			energy:!this.state.energy,
-			open:true,
-			license:license
-		},this.returnProps())
-		
-	}
+	//清空输入信息
 	cleanPro=()=>{
+		console.log('清空')
 		this.setState({
-			license:['','','','','','','',''],
+			license:['-','-','-','-','-','-','-','-'],
 			inputKey:0,//输入的序号
+		},()=>{
+			console.log(this.state.license,this.state.inputKey)
+			this.returnProps()
 		})
-		this.returnProps()
 	}
 	closePro=()=>{
 		this.setState({
@@ -115,39 +232,27 @@ class Chepai extends Component {
 	}
 
 	render() {
-		return (<div style={this.props.style||{position: 'fixed', height: '100%', width: '100%', top: 0}}>    
-			<ul className='clearfix ul_input'>
-				{this.state.license.map((value,i)=>{
-					if(!this.state.energy && i === 7){
-						return;
-					}
-					return(<li onClick={()=>this.clickBox(i)} className='input_pro' style={{width:this.state.energy?'12.5%':'14%'}} key={i}>
-							<span  style={{width:this.state.energy?'32px':'40px',border:`${this.state.inputKey===i?'1px solid #ff1000':'1px solid #ccc'}`}} className='input_pp input_zim'>
-								{value}
-							</span>
-						</li>)
-				})}
-			</ul>
-			{this.state.open?<div>	
+		return (<div className={"Chepai"} style={this.props.style||{height: '100%', width: '100%', top: 0}}>
+			{this.state.open?<div>
 				{this.state.inputKey===0?
-					<ul className='clearfix ul_pro' >
+					<ul className='clearfix ul_pro' style={{bottom:this.props.bottom||'50px',zIndex:this.state.zIndex}}>
 						{inputContent.provinces.map((value,i)=>{
-							return(<li key={i}><span onClick={()=>this.chooseProvince(value)}>{value}</span></li>)
+							return(<li key={i}><span name="chepaiInput" onClick={()=>this.chooseProvince(value)}>{value}</span></li>)
 						})}
-						<li className='li_close'><span onClick={()=>this.closePro()}>关闭</span></li>
-						<li className='li_clean'><span onClick={()=>this.cleanPro()}>清空</span></li>
+						<li   className='li_close'><span onClick={()=>this.closePro()}>关闭</span></li>
+						<li name="chepaiInput" className='li_clean'><span name="chepaiInput" onClick={()=>this.cleanPro()}>清空</span></li>
+						<li   className='li_delete'><span className={"delete"} onClick={()=>this.choosekey("删除")}></span></li>
 					</ul>
-					:<ul className='clearfix ul_keybord' >
+					:<ul className='clearfix ul_keybord' style={{bottom:this.props.bottom||'50px',zIndex:this.state.zIndex}}>
 						{inputContent.keyNums.map((value,i)=>{
-							return(<li key={i} className={
+							return(<li key={i} name="chepaiInput" className={
 								i<10?'li_num':i===20?'li_zm ikey20':i>28?'li_zm li_w':'li_zm'
-							}><span onClick={()=>this.choosekey(value)}>{value}</span></li>)
+							}><span name="chepaiInput" className={value==="删除"?"delete":""} onClick={()=>this.choosekey(value)}>{value!=="删除"?value:""}</span></li>)
 						})}
 					</ul>
-				}	
-				</div>:''}
-				<div className='energy_choose' onClick={()=>this.selectEnergy()} ><img src={this.state.selectEnergy} className='choose' alt=''/><span>新能源汽车</span></div>
-        </div>)
+				}
+			</div>:''}
+		</div>)
 	}
 }
 
--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,22 +1,108 @@
 import React, { Component } from 'react';
 import Chepai from './Chepai';
 import './index.css';
-
+import add_1 from "../../dist/img/add-1.svg";
 class Index extends Component {
 	constructor(props) {
 		super(props);
 		this.state={
-			open:true,
+			parkNumberError:false,
+			energy:false,
+			openCheipai:true,
+			licenseNum:'',
+			inputKey:0,
+			licenseTest:false,//检测车牌 是否正确：正确：true
+			licenseList_switch:false,//开关
+			licenseList_show_item:[],
 		}
 	}
 
-	onChange=(val)=>{
-		console.log(val)
+	chepaiOnchange=(v)=>{
+		console.log(v)
+		let licenseTest;
+		if(/^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$/.test(v.license)){
+			licenseTest=true;
+		}else{
+			licenseTest=false;
+		}
+		this.setState({
+			licenseNum:v.license,
+			openCheipai:v.open,
+			inputKey:v.inputKey,
+			licenseTest:licenseTest,
+		},()=>console.log("车牌是否监测通过："+this.state.licenseTest))
+	}
+	isEnergy=()=>{
+		let inputKey=this.state.inputKey;
+		if(this.state.energy&&inputKey===7){
+			this.setState({
+					licenseNum:this.state.licenseNum.split("").length===8?this.state.licenseNum.substring(0,this.state.licenseNum.length-1):this.state.licenseNum,
+					inputKey:6
+				},
+				()=>console.log(this.state.inputKey))
+		}
+		if(this.state.energy&&inputKey<7){
+			this.setState({
+					licenseNum:this.state.licenseNum.split("").length===8?this.state.licenseNum.substring(0,this.state.licenseNum.length-1):this.state.licenseNum,
+					inputKey:this.state.inputKey
+				},
+				()=>console.log(this.state.inputKey))
+		}
+		if(!this.state.energy&&inputKey===6){
+			this.setState({
+					inputKey:7
+				},
+				()=>console.log(this.state.inputKey))
+		}
+		this.setState({energy:!this.state.energy})
+	}
+	openCheipaiInput=()=>{
+		this.setState({
+			openCheipai:true,
+		},()=>console.log(this.state.openCheipai))
 	}
 
 	render() {
 		let style={position: 'fixed', height: '100%', width: '100%', top: 0}
-		return (<Chepai style={style} onChange={val=>this.onChange(val)} open={this.state.open}/>)
+		return (<div className={"Index"}>
+				<div className="chepai-box" onClick={()=>{this.openCheipaiInput()}}>
+					<div className={`chepai-box-item ${this.state.inputKey===0?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:0})}
+					>{this.state.licenseNum.split("")[0]!=="-"?this.state.licenseNum.split("")[0]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===1?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:1})}
+					>{this.state.licenseNum.split("")[1]!=="-"?this.state.licenseNum.split("")[1]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===2?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:2})}
+					>{this.state.licenseNum.split("")[2]!=="-"?this.state.licenseNum.split("")[2]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===3?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:3})}
+					>{this.state.licenseNum.split("")[3]!=="-"?this.state.licenseNum.split("")[3]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===4?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:4})}
+					>{this.state.licenseNum.split("")[4]!=="-"?this.state.licenseNum.split("")[4]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===5?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:5})}
+					>{this.state.licenseNum.split("")[5]!=="-"?this.state.licenseNum.split("")[5]:""}</div>
+					<div className={`chepai-box-item ${this.state.inputKey===6?"activity":""}`}
+						 onClick={()=>this.setState({inputKey:6})}
+					>{this.state.licenseNum.split("")[6]!=="-"?this.state.licenseNum.split("")[6]:""}</div>
+					<div className={`energy chepai-box-item ${this.state.inputKey===7?"activity":""}`} onClick={()=>this.isEnergy()}>
+						{this.state.energy?this.state.licenseNum.split("")[7]:<div className={"cen"}>
+							<img src={add_1} alt=""/>
+							<div className={"text"}>新能源</div>
+						</div>}
+					</div>
+				</div>
+				<Chepai open={this.state.openCheipai}
+						energy={this.state.energy}
+						onChange={v=>this.chepaiOnchange(v)}
+						inputKey={this.state.inputKey}
+						license={this.state.licenseNum}
+						bottom={"0px"} zIndex="101" style={{height:"auto"}}
+				></Chepai>
+			</div>
+			)
 	}
 }
 
